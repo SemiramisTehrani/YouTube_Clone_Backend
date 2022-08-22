8bfe887429796f24f97b1e02200d23e2ad588b7c
unknown <semtehrani@yahoo.com>
2022-08-21 17:18:29 -0700

    more folders in components

---
+++ frontend/src/components/RelatedVideos/RelatedVideos.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import keys from "../../API_keys.json";
+import "../RelatedVideos/RelatedVideos.css";
+
+const RelatedVideos = (props) => {
+  const [listRelatedVideos, setListRelatedVideos] = useState([]);
+
+  async function getRelatedVideos() {
+    console.log("Called getRelatedVideos successfully");
+    if (props.videoId) {
+      let response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${props.videoId}&type=video&key=${keys.googleAPIkey}&part=snippet`
+      );
+      setListRelatedVideos(response.data.items);
+      console.log("related video list", response.data.items);
+    } else {
+      console.log("No current video ID");
+    }
+  }
+
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      getRelatedVideos();
+    }
+    return () => (mounted = false);
+  }, [props.videoId]);
+
+  function handleSubmit(videoId) {
+    console.log(videoId);
+    props.setVideoId(videoId);
+  }
+
+  return (
+    <div>
+      <ul>
+        {listRelatedVideos.map((video, index) => {
+          if (video.snippet) {
+            return (
+              <li key={index}>
+                <button
+                  type="button"
+                  onClick={() => handleSubmit(video.id.videoId)}
+                >
+                  <img
+                    src={video.snippet.thumbnails.default.url}
+                    alt="thumbnails"
+                  ></img>
+                </button>
+              </li>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default RelatedVideos;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  <Router>
+      <App />
+  </Router>,
+  document.getElementById("root"));
